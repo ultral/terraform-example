3d94cf08d99bf4203d44833dca671d8c602c4d7f
Lev Goncharov <lev@goncharov.xyz>
2017-10-23 23:01:10 +0300

    don't use kubectl & docker from cetnos repo

--- serverspec/spec/localhost/minikube_spec.rb
+++ serverspec/spec/localhost/minikube_spec.rb
@@ -4,3 +4,8 @@ describe file('/usr/local/bin/minikube') do
   it { should be_file }
   it { should be_mode '755' }
 end
+
+describe file('/usr/local/bin/kubectl') do
+  it { should be_file }
+  it { should be_mode '755' }
+end
